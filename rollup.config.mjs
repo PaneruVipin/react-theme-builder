ac77662abe20abad6a276eea1e1e79f26a69e913
PaneruVipin <paneruvipinbusiness@gmail.com>
2023-04-04 11:42:17 +0530

    first commit

---
+++ rollup.config.mjs
@@ -0,0 +1,27 @@
+import babel from "rollup-plugin-babel";
+import { terser } from "rollup-plugin-terser";
+import postcss from "rollup-plugin-postcss";
+import image from "@rollup/plugin-image";
+
+export default {
+  input: "src/App.js",
+  output: [
+    {
+      file: "dist/index.js",
+      format: "cjs",
+      // name: 'MyApp',
+      sourcemap: true,
+      plugins: [terser()],
+    },
+  ],
+  external: ["react", "react-dom"],
+  plugins: [
+    babel({
+      exclude: "node_modules/**",
+    }),
+    postcss({
+      extract: true,
+    }),
+    image(),
+  ],
+};
